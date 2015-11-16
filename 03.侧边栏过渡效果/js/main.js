d11b9de11db24742bf81f1ad1b4fcdabf891a275
xiaomucool-home <a59769507@hotmail.com>
2015-11-16 21:46:53 +0800

    update 05

--- 03.侧边栏过渡效果/js/main.js
+++ 03.侧边栏过渡效果/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
-  
+  $(".btns button").each(function(index){
+  	$(this).click(function(){
+  		$("body").removeClass();
+  		$("body").addClass("now");
+  		$("body").addClass("effect-"+index);
+  	});
+  });
+  $(".mask").click(function(){
+  	$("body").removeClass("now");
+  });
 });
